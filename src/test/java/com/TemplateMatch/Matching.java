27743b0bc9f43f5189e3de572cfd16993be40ab8
wangli <wangli_cq@hotmail.com>
2016-12-27 18:15:31 +0800

    [Li]Add test

--- src/test/java/com/TemplateMatch/Matching.java
+++ src/test/java/com/TemplateMatch/Matching.java
@@ -4,66 +4,123 @@ import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
-
-// http://docs.opencv.org/2.4/doc/tutorials/imgproc/histograms/template_matching/template_matching.html#template-matching
 public class Matching {
 
-    public static int[] getMatchingLocation(String... args) throws Exception {
-        // Read source image and template image
+    public static int[] getMatchingLocation1(String... args) throws Exception {
+        // create input and output mat
         Mat src = Imgcodecs.imread(args[0], 0);
         Mat tmp = Imgcodecs.imread(args[1], 0);
 
         // Create the result matrix
+        Mat result = createResultMatrix(src, tmp);
+
+        // Match Template Function from OpenCV
+        Core.MinMaxLocResult mmr = matchAndFindLoc(src, tmp, result, Imgproc.TM_SQDIFF_NORMED);
+
+        // Got location
+        Point matchLoc = getCorrectLoc(mmr, Imgproc.TM_SQDIFF_NORMED);
+
+        //show what we got
+        getMatchArea(src, tmp,  matchLoc, args);
+
+        //return location
+
+        int myLocation[] = getClickLocation(matchLoc, tmp);
+
+        System.out.println("x= " + myLocation[0]);
+        System.out.println("y= " + myLocation[1]);
+
+        return myLocation;
+    }
+
+
+    public static int[] getMatchingLocation2(String... args) throws Exception {
+        // create input and output mat
+        Mat src = readImage(0, args);
+        Mat tmp = readImage(1, args);
+        // Create the result matrix
+        Mat result = createResultMatrix(src, tmp);
+
+        // Match Template Function from OpenCV
+        Core.MinMaxLocResult mmr = matchAndFindLoc(src, tmp, result, Imgproc.TM_CCOEFF_NORMED);
+
+        // Got location
+        Point matchLoc = getCorrectLoc(mmr, Imgproc.TM_CCOEFF_NORMED);
+
+        //got match percentage
+        double matchPercentage = getMatchPercentage(mmr, Imgproc.TM_CCOEFF_NORMED);
+
+        if(matchPercentage >= 0.95) {
+            //show what we got
+            getMatchArea(src, tmp,  matchLoc, args);
+
+            int myLocation[] = getClickLocation(matchLoc, tmp);
+
+            System.out.println("x= " + myLocation[0]);
+            System.out.println("y= " + myLocation[1]);
+
+            return myLocation;
+        }
+        else {
+            return null;
+        }
+    }
+
+
+    private static Mat readImage(int index, String... args){
+        return Imgcodecs.imread(args[index], 0);
+    }
+
+    private static Mat createResultMatrix(Mat src, Mat tmp) {
         int result_cols = src.cols() - tmp.cols() +1;
         int result_rows = src.rows() - tmp.rows() +1;
         Mat result = new Mat(result_cols,result_rows, CvType.CV_32FC1);
+        return result;
+    }
+
+    //get min max location
+    private static Core.MinMaxLocResult matchAndFindLoc(Mat src, Mat tmp, Mat result, int method) {
+        Imgproc.matchTemplate(src, tmp, result, method);
+        Core.MinMaxLocResult mmr = Core.minMaxLoc(result);
+        return mmr;
+    }
 
 
-
-        // Match Template Function from OpenCV
-        int method = Imgproc.TM_SQDIFF_NORMED; // select a matching method
-        //*****     matching and get the result matrix     ********
-
-        //****     get the min and max  values and their locations    *****
-        Core.MinMaxLocResult mmr = null;
-
-
-
-        // Got location and match percentage
-        Point matchLoc;
-        double matchPercentage;
+    private static Point getCorrectLoc(Core.MinMaxLocResult mmr, int method) {
         if(method == Imgproc.TM_SQDIFF_NORMED || method == Imgproc.TM_SQDIFF){
-            matchLoc =  mmr.minLoc;
-            matchPercentage = 1-mmr.minVal;
+            return mmr.minLoc;
         }
         else {
-            matchLoc =  mmr.maxLoc;
-            matchPercentage = mmr.maxVal;
+            return mmr.maxLoc;
         }
 
+    }
 
-        //show what we got
-        //***** get the endPoint  *****
-        Point endPoint;
-        //***** draw a rectangle from the matchLoc to endPoint;   *****
+    private static double getMatchPercentage(Core.MinMaxLocResult mmr, int method) {
+        if(method == Imgproc.TM_SQDIFF_NORMED || method == Imgproc.TM_SQDIFF){
+            return 1 - mmr.minVal;
+        }
+        else {
+            return mmr.maxVal;
+        }
+    }
 
-        //write the compare result
-        Imgcodecs.imwrite("compare/lena_face.png", src);
+    //draw
+    private static void getMatchArea(Mat src, Mat tmp, Point matchLoc, String... args) {
+        Point endPoint = new Point(matchLoc.x + tmp.cols(), matchLoc.y + tmp.rows());
+        Imgproc.rectangle(src, matchLoc, endPoint, new Scalar(0,255,0));
+
+        Imgcodecs.imwrite(args[2],src);
+    }
 
 
-
-        //return location
+    private static int[] getClickLocation(Point matchLoc, Mat tmp) {
         Double x1 = matchLoc.x;
         Double y1 = matchLoc.y;
         int x = x1.intValue() + tmp.cols()/2;
         int y = y1.intValue() + tmp.rows()/2;
-
-        System.out.println(matchPercentage);
-        if(matchPercentage > 0.95) {
-            return new int[]{x, y};
-        }
-        else{
-            return null;
-        }
+        return new int[]{x, y};
     }
+
+
 }
--- src/test/java/com/TemplateMatch/WechatTest.java
+++ src/test/java/com/TemplateMatch/WechatTest.java
@@ -5,12 +5,15 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.opencv.core.*;
+import org.opencv.core.Core;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
 
 public class WechatTest {
     AppiumDriver wd;
@@ -26,22 +29,32 @@ public class WechatTest {
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     }
 
+
     @Test
-    public void login() {
-        wd.findElement(By.xpath("//android.view.View[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.RelativeLayout[1]/android.widget.Button[1]")).click();
+    public void opencvLoginTest() throws Exception {
+        TakeScreenshot screenshot = new TakeScreenshot();
+        Matching matching = new Matching();
+        wd.findElement(By.xpath("//android.view.View[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.RelativeLayout[1]/android.widget.Button[2]"));
+        screenshot.take(wd, "screenshot_login");
+        int location[] = matching.getMatchingLocation1("screenshots/screenshot_login.png", "temp/Login.png", "compare/login_compared.png");
+        wd.tap(1, location[0], location[1], 100);
         wd.findElement(By.xpath("//android.widget.FrameLayout[1]/android.view.View[1]/android.widget.FrameLayout[2]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.LinearLayout[2]/android.widget.Button[2]")).click();
-        wd.findElement(By.xpath("//android.widget.FrameLayout[1]/android.view.View[1]/android.widget.FrameLayout[2]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]/android.widget.EditText[1]")).sendKeys("acount");
+        wd.findElement(By.xpath("//android.widget.FrameLayout[1]/android.view.View[1]/android.widget.FrameLayout[2]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]/android.widget.EditText[1]")).sendKeys("account");
         wd.findElement(By.xpath("//android.widget.FrameLayout[1]/android.view.View[1]/android.widget.FrameLayout[2]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[2]/android.widget.EditText[1]")).sendKeys("password");
         wd.findElement(By.xpath("//android.widget.FrameLayout[1]/android.view.View[1]/android.widget.FrameLayout[2]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.Button[1]")).click();
-        wd.findElement(By.xpath("//android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[2]/android.widget.LinearLayout[1]/android.widget.Button[1]")).click();
+        wd.findElement(By.xpath("//android.widget.FrameLayout[1]/android.view.View[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.support.v4.view.ViewPager[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.ListView[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[2]")).click();
     }
-
 
     @Test
-    public void opencvLogin() throws Exception {
-
+    public void opencvGetRedpacketTest() throws Exception {
+        opencvLoginTest();
+        ScheduledExecutorService executorService = new ScheduledThreadPoolExecutor(1);
+        Task task = new Task(wd);
+        executorService.scheduleWithFixedDelay(task, 5000, 5000, TimeUnit.MILLISECONDS);
+        while (!executorService.isShutdown()) {
+            Thread.sleep(1000);
+        }
     }
-
 
     @After
     public void shutDown() throws Exception{
